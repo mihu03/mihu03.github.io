3827dc3308a3d852d6da50abf891af2fd587ae0a
miat24 <13224797+miat24@user.noreply.gitee.com>
2024-03-21 17:09:37 +0800

    deploy

---
+++ assets/index.html-6eb673ed.js
@@ -0,0 +1,16 @@
+import{_ as t}from"./background-4282d3ab.js";import{_ as o}from"./plugin-vue_export-helper-c27b6911.js";import{o as n,c as i,a,d as e}from"./app-459071b1.js";const r={},s=e("div",{style:{"text-align":"center"}},[e("img",{src:t,width:"3000",heigh:"600"})],-1),l=e("br",null,null,-1),c=e("div",{style:{"text-align":"justify","font-family":"Times New Roman","font-size":"20px",color:"black"}},[e("h1",{style:{"text-align":"center","font-size":"30px",color:"black"}},"Venue"),e("br"),e("h2",{style:{"font-weight":"bold","font-size":"20px"}},"The routes to the meeting location are as follows:")],-1);function f(h,d){return n(),i("div",null,[s,l,c,a(` <object data="/Airport_guide_maps.pdf.pdf" type="application/pdf" width="100%" height="1380">
+　　<iframe width='100%' height='1380' frameborder='1' name='Route' src="/Airport_guide_maps.pdf" ></iframe>
+</object>
+<h2 style="font-weight:bold; font-size: 20px;">The  venue for ICACI2023 is Best Western Premier Gangnam</h2>
+<ol>
+        <ul type="disc" style="font-size: 16px;">
+            <li>Address: 205-9 Nonhyun-Dong, Gangnam-Gu Seoul, 135-010 Korea (South)</li>
+            <li> Webpage: <a href="https://www.royal.co.kr/2014_renew/en/" style="color: #27ae60;" target="_blank">http://en.bestwesterngangnam.com/?redirect=no</a></li>
+        </ul>
+</ol>
+<br/>
+<p style="text-align: justify;font-family: Times New Roman;font-size: 18px;text-align:justify; color: black">or choose the nearby hotels. There are various hotels in Gangnam, a well-known tourist district.</p>
+<br/>
+<object data="/Hotel_information.pdf" type="application/pdf" width="100%" height="1380">
+　　<iframe width='100%' height='1380' frameborder='1' name='Route' src="/Hotel_information.pdf" ></iframe>
+</object> `)])}const u=o(r,[["render",f],["__file","index.html.vue"]]);export{u as default};
---
+++ assets/index.html-eb0b78e6.js
@@ -0,0 +1,56 @@
+import{_ as t}from"./background-4282d3ab.js";import{_ as l}from"./plugin-vue_export-helper-c27b6911.js";import{o,c as a,a as n,d as e}from"./app-459071b1.js";const i={},r=e("div",{align:"center"},[e("img",{src:t,width:"3000",heigh:"600"})],-1),s=e("h1",{style:{"text-align":"center","font-size":"30px",color:"black"}},"Registration",-1);function c(d,p){return o(),a("div",null,[r,s,n(` <p style="text-align: left; font-family: Times New Roman;font-size: 20px; color: black; font-weight:bold;">Registration Fees (Costs in USD):</p>
+<div class="center" style="font-family: Times New Roman;font-size: 16px; color: black">
+
+<table style="margin:auto; color: black" width='100%'>
+    <tr style="border: 1px solid black; text-align: center;">
+      <td style="border: 1px solid black;color: black;width:240px">CATEGORY</td>
+      <td style="border: 1px solid black;color: black;width:240px">Registration by April 7, 2023</td>
+      <td style="border: 1px solid black;color: black;width:240px">Registration after April 7, 2023</td>
+   </tr>
+   <tr style="border: 1px solid black; text-align: center;">
+      <td style="border: 1px solid black;color: black;width:240px">Paper Registration Fee</td>
+      <td style="border: 1px solid black;color: black;width:240px">USD 550 </td>
+      <td style="border: 1px solid black;color: black;width:240px">USD 605</td>
+   </tr>
+   <tr style="border: 1px solid black; text-align: center;">
+      <td style="border: 1px solid black;color: black;width:240px">No Paper Registration Fee</td>
+      <td style="border: 1px solid black;color: black;width:240px">USD 400</td>
+      <td style="border: 1px solid black;color: black;width:240px">USD 440</td>
+   </tr>
+   <tr style="border: 1px solid black; text-align: center;">
+      <td style="border: 1px solid black;color: black;width:240px">Extra page fee</td>
+      <td style="border: 1px solid black;color: black;width:240px" colspan="2">Extra page fee at USD 100 per page (Max. 2 pages for each paper)</td>
+   </tr>
+   <tr style="border: 1px solid black; text-align: center;">
+      <td style="border: 1px solid black;color: black;width:240px">Extra paper fee</td>
+      <td style="border: 1px solid black;color: black;width:240px" colspan="2">2nd paper of the same author at USD 450</td>
+   </tr>
+   <tr style="border: 1px solid black; text-align: center;">
+      <td style="border: 1px solid black;color: black;width:240px">Extra banquet tickets</td>
+      <td style="border: 1px solid black;color: black;width:240px" colspan="2">USD 50 for each extra</td>
+   </tr>
+</table>
+<br/>
+</div>
+
+<div style="font-family: Times New Roman;font-size: 16px; color: black">
+
+<p style="text-align: left; font-family: Times New Roman;font-size: 20px; color: black; font-weight:bold;">Registration and Refund Policy:</p>
+<ul style="text-align: left; font-family: Times New Roman;font-size: 16px; color: black;">
+    <li>An accepted paper will be published in the proceedings <b>ONLY</b> if at least one author registers for the conference.</li>
+    <li>An accepted paper must have a <b>registration completed by March 30, 2023</b>, otherwise it may be considered to be withdrawn.</li>
+    <li>One single extra paper for the same author may be included in a registration, provided the extra paper fee is paid.</li>
+    <li>MiAT2024 provides onsite and online presentation. The authors can choose via the payment link.</li>
+    <li> Refund Policy: There will be no refunds to the authors.</li>
+</ul>
+<br/>
+<p style="text-align: left; font-family: Times New Roman;font-size: 20px; color: black; font-weight:bold;">Payment Methods:</p>
+<p style="text-align: left; font-family: Times New Roman;font-size: 18px; color: black; font-weight:bold;">International Payments:</p>
+<ul style="text-align: left; font-family: Times New Roman;font-size: 16px; color: black;">
+    <li>International payment via credit card.</li>
+    <li>If you prefer to pay via wire transfer please email us at <span style="color: #27ae60">icaci202305@gmail.com</span>.</li>
+</ul>
+<p style="text-align: left; font-family: Times New Roman;font-size: 20px; color: black;">Please click <a style="text-align: center; color: #27ae60" href="https://icaci2023p.com/Register" target="_blank">
+   here</a>(https://icaci2023p.com/Register) to pay.</p>
+   
+</div> `)])}const m=l(i,[["render",c],["__file","index.html.vue"]]);export{m as default};
